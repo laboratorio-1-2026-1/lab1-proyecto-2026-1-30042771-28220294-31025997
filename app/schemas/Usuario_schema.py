b45e868e70f7e8c456e9c7b2f361ab4ed63109bd
Ricgonzalez89 <ricardoa.gonzalez04@gmail.com>
2026-05-12 13:08:35 -0400

    Attribute clave_hash changed to clave to avoid confusions in Usuario_schema

--- app/schemas/Usuario_schema.py
+++ app/schemas/Usuario_schema.py
@@ -11,7 +11,7 @@ class Usuario_Create(Usuario_Base):
     """
     Esquema para la creacion de usuarios nuevos.
     """
-    clave_hash: str = Field(..., min_length=8, description="Clave de usuario (con 8 caracteres minimo).")
+    clave: str = Field(..., min_length=8, description="Clave de usuario (con 8 caracteres minimo).")
 
 class Usuario_Update(BaseModel):
     """
@@ -19,7 +19,7 @@ class Usuario_Update(BaseModel):
     """
     id_rol: int | None = Field(..., ge=1, description="Identificador del rol del usuario.")
     correo: EmailStr | None = Field(..., min_length=5, max_length=40, description="Correo de usuario.")
-    clave_hash: str | None = Field(..., min_length=8, description="Clave de usuario (con 8 caracteres minimo).")
+    clave: str | None = Field(..., min_length=8, description="Clave de usuario (con 8 caracteres minimo).")
     status_usuario: bool | None = Field(default=True, description="Usuario activo (True o False).")
 
 class Usuario_Out(BaseModel):
