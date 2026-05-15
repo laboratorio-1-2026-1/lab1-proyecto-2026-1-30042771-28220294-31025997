421d7d6f410b89d5d049defe3b93ddb51baab4bf
Ricgonzalez89 <ricardoa.gonzalez04@gmail.com>
2026-05-14 23:55:03 -0400

    Inclusion de archivo con la estructura estandar de errores

---
+++ app/core/errors.py
@@ -0,0 +1,48 @@
+from datetime import datetime, UTC
+
+class App_Exception(Exception):
+    """
+    Clase base para estandarizar el manejo de los diversos errores posibles en el sistema.
+    """
+    def __init__(self, status_code=500, error="Internal Server Error", message="Falla en el sistema", internal_code="API_ERROR"):
+        self.status_code = status_code
+        self.error = error
+        self.message = message
+        self.internal_code = internal_code
+        self.timestamp = datetime.now(UTC).isoformat()
+        super().__init__(self.message)
+
+class Bad_Request_Exception(App_Exception):
+    """
+    Error 400 - BAD REQUEST, ante fallas en el cuerpo de las solicitudes.
+    """
+    def __init__(self, message, internal_code="BAD_REQUEST_EXCEPTION"):
+        super().__init__(400, "Bad Request", message, internal_code)
+
+class Unauthorized_Exception(App_Exception):
+    """
+    Error 401 - UNAUTHORIZED, ante falta de Token de acceso o Tokens expirados.
+    """
+    def __init__(self, message, internal_code="UNAUTHORIZED_EXCEPTION"):
+        super().__init__(401, "Unauthorized", message, internal_code)
+
+class Forbidden_Exception(App_Exception):
+    """
+    Error 403 - FORBIDDEN, ante la falta de permisos suficientes para realizar alguna accion.
+    """
+    def __init__(self, message, internal_code="FORBIDDEN_EXCEPTION"):
+        super().__init__(403, "Forbidden", message, internal_code)
+
+class Not_Found_Exception(App_Exception):
+    """
+    Error 404 - NOT FOUND, ante la imposibilidad de encontrar recursos solicitados.
+    """
+    def __init__(self, message, internal_code="NOT_FOUND_EXCEPTION"):
+        super().__init__(404, "Not Found", message, internal_code)
+
+class Conflict_Exception(App_Exception):
+    """
+    Error 409 - CONFLICT, ante reglas de negocio fallidas.
+    """
+    def __init__(self, message, internal_code="CONFLICT_EXCEPTION"):
+        super().__init__(409, "CONFLICT", message, internal_code)
