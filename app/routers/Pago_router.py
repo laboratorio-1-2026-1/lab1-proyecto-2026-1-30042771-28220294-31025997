d566eaac6cf638e5334eaaabf18c86560cd2b999
Genesis Carrasco <genesis72cr@gmail.com>
2026-05-19 21:53:45 -0400

    implementar paginacion  en el historial de pagos, toque los archivos pago_router, pago_service, pagoMembresia_repository

--- app/routers/Pago_router.py
+++ app/routers/Pago_router.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 
 from app.database.session import get_db
-from app.core.utils import Role_Checker, get_current_user  # Tu middleware perimetral de roles
+from app.core.utils import Role_Checker, get_current_user  
 from app.schemas.PagoMembresia_schema import PagoMembresia_Create, PagoMembresia_Out
 from app.services.Pago_service import Pago_Service
 
@@ -35,7 +35,9 @@ async def registrar_pago_de_membresia(
     servicio = Pago_Service(session)
     return await servicio.registrar_pago_membresia(pago_in)
 
-
+#------------------------------------ 
+#ENDPOINT MODIFICADO CON PAGINACIÓN
+#------------------------------------
 @router.get(
     "/historial", 
     response_model=List[PagoMembresia_Out],
@@ -43,13 +45,13 @@ async def registrar_pago_de_membresia(
     dependencies=[Depends(permiso_financiero), Depends(get_current_user)]
 )
 async def consultar_historial_de_pagos(
-    session: AsyncSession = Depends(get_db)
+    session: AsyncSession = Depends(get_db),
+    page: int = Query(default=1, ge=1, description="Número de la página a consultar"), 
+    size: int = Query(default=10, ge=1, le=100, description="Registros de pagos por página que se desea") 
 ):
     """
-    Permite al departamento de finanzas y administración auditar 
-    el historial de transacciones e ingresos del gimnasio.
+    Permite al rol de finanzas y administración revisar 
+    el historial de transacciones e ingresos del gimnasio de forma paginada.
     """
-    # En el futuro mapearías al repositorio de pagos:
-    # servicio = Pago_Service(session)
-    # return await servicio.obtener_todos_los_pagos()
-    return []
+    servicio = Pago_Service(session)
+    return await servicio.obtener_todos_los_pagos(page=page, size=size) 
