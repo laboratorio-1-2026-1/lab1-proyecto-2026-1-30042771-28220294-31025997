0168d443c9f1395946b43cae41a92ac99be7babe
Genesis Carrasco <genesis72cr@gmail.com>
2026-05-29 22:41:59 -0400

    Endpoint gestion de membresias, si la membresia esta activa, vencida o por vencer, se estandarizo en enums

--- app/core/enums.py
+++ app/core/enums.py
@@ -4,3 +4,8 @@ class TipoPagoEnum(str, Enum):
     ADQUISICION = "Adquisición de Plan"
     RENOVACION = "Renovación de Plan"
     TIENDA = "Compra de Producto"
+
+class ActividadMembresiaEnum(str, Enum):
+    ACTIVA = "Activa"
+    VENCIDA = "Vencida"
+    POR_VENCER = "Por Vencer"
--- app/schemas/Membresia_schema.py
+++ app/schemas/Membresia_schema.py
@@ -1,15 +1,16 @@
 from pydantic import BaseModel, Field, ConfigDict
 from datetime import date
+from app.core.enums import ActividadMembresiaEnum
 
 class Membresia_Base(BaseModel):
     """
     Esquema base para la validacion de membresias en el sistema.
     """
-    cedula_cliente: str = Field(..., min_length=7, max_length=20, description="Cedula del cliente asociado.")
+    cedula_cliente: str = Field(..., min_length=7, max_length=20, description="Cedula del cliente.")
     id_plan: int = Field(..., ge=1, description="ID del plan elegido.")
     fecha_inicio: date = Field(..., description="Fecha en la que inicia la membresia.")
     fecha_venci: date = Field(..., description="Fecha de vencimiento de la membresia.")
-    actividad_membre: str = Field(..., max_length=20, description="Descripcion de la actividad (ej: 'Pesas', 'Zumba').")
+    actividad_membre: ActividadMembresiaEnum = Field(..., max_length=20, description="Descripcion de la actividad de la membresia ('Activa', 'Vencida', 'Por Vencer')")
 
 class Membresia_Create(Membresia_Base):
     """
@@ -26,14 +27,14 @@ class Membresia_Update(BaseModel):
     id_plan: int | None = Field(None, ge=1)
     fecha_inicio: date | None = Field(None)
     fecha_venci: date | None = Field(None)
-    actividad_membre: str | None = Field(None, max_length=20)
+    actividad_membre: ActividadMembresiaEnum | None = Field(None, max_length=20)
     status_membresia: bool | None = Field(None)
 
 class Membresia_Out(Membresia_Base):
     """
     Esquema para validar la salida de datos de membresias.
     """
-    id_membresia: int
+    id_membresia: int 
     status_membresia: bool
 
     model_config = ConfigDict(from_attributes=True) 
